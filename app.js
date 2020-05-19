e2372323cf95ca47bc2a50ff99c99b05c6d46056
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-19 00:14:41 -0400

    now you can add winning score in the begining

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var scores, roundScore, dice, activePlayer, gamePlaying, prevDice;
+var scores, roundScore, dice, activePlayer, gamePlaying, prevDice, winningScore;
 
 init();
 
@@ -6,13 +6,11 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying){
         prevDice = dice;
         dice = Math.floor(Math.random() * 6) + 1;
-        console.log(activePlayer + " "+ prevDice+" "+dice + " "+scores);
         document.getElementById('image').src = "img/dice-"+dice+".png";
         document.querySelector('.dice').style.display = 'block';
         if(prevDice === 6 && dice === 6) {
-            roundScore = 0;
             scores[activePlayer] = 0;
-            document.querySelector('#score-'+activePlayer).innerHTML = '0';
+            document.querySelector('#score-'+activePlayer).textContent = '0';
             nextPlayer();
         }
         if(dice === 1){
@@ -31,7 +29,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     if(gamePlaying){
         scores[activePlayer] += roundScore;
         document.getElementById("score-"+activePlayer).innerHTML = scores[activePlayer];
-        if(scores[activePlayer] >= 100){
+        if(scores[activePlayer] >= winningScore){
             document.getElementById("name-"+activePlayer).innerHTML = "Winner!";
             document.querySelector('.player-'+ activePlayer +'-panel').classList.add('winner');
             document.querySelector('.player-'+ activePlayer +'-panel').classList.remove('active');
@@ -48,7 +46,7 @@ document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
     roundScore = 0;
-    document.getElementById("current-"+activePlayer).innerHTML = roundScore;
+    document.querySelector("#current-"+activePlayer).textContent = roundScore;
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
@@ -76,3 +74,16 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
+
+document.getElementById('limit-btn').addEventListener('click', function() {
+    winningScore = document.getElementById('limit').value;
+    document.querySelector('.btn-new').classList.remove('hide');
+    document.querySelector('.btn-roll').classList.remove('hide');
+    document.querySelector('.btn-hold').classList.remove('hide');
+    document.querySelectorAll('.dull').forEach(function(cls) {
+        cls.classList.remove('dull');
+    })
+    document.getElementById('title').classList.add('hide');
+    document.querySelector('#limit-btn').style.display = 'none';
+    document.querySelector('#limit').style.display = 'none';
+});
